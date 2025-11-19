888e4bc42ac91f808ebce62388fa20b6cc851545
mamaevbakh <mr.mamaev01@mail.ru>
2025-11-19 05:00:34 +0500

    Suspense added to theme provider tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,10 +2,11 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { Suspense } from "react"
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}><Suspense fallback={<div>Loading theme...</div>}>{children}</Suspense></NextThemesProvider>
 }
