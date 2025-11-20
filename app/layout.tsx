d44684b96a866d79c35b147ca3d4c7957bca75e8
Bakhtiyor Mamaev <bakh@MacBook-Pro-Bakhtiyor.local>
2025-11-21 02:34:26 +0500

    Tweakcn script added for demo shop

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          async
+          crossOrigin="anonymous"
+          src="https://tweakcn.com/live-preview.min.js"
+        />
+      </head>
       <body
         className={`${GeistSans.variable} font-sans antialiased`}
       >
