eca9097e5897784be2310bb8d611475eecbe21af
Bakhtiyor Mamaev <bakh@MacBook-Pro-Bakhtiyor.local>
2025-11-22 15:31:01 +0500

    Badge is not used in layout

--- app/[...slug]/layout.tsx
+++ app/[...slug]/layout.tsx
@@ -1,8 +1,5 @@
-// app/[...slug]/layout.tsx
-
-import { MadeByKraftaBadge } from "@/components/krafta/made-by-krafta";
 import type { ReactNode } from "react";
 
 export default function CatalogLayout({ children }: { children: ReactNode }) {
-  return <div className="min-h-screen bg-white dark:bg-secondary-background text-foreground">{children}<MadeByKraftaBadge /></div>;
+  return <div className="min-h-screen bg-white dark:bg-secondary-background text-foreground">{children}</div>;
 }
